958f5cca7284739464bc27bacead3ceb998bc461
Jose Torreblanca <josewhitetower@gmail.com>
2018-10-28 15:26:58 +0100

    added router

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About =() =>{
+    return(
+        <div className="container">
+            <h4 className="center">About</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis voluptates placeat obcaecati sequi neque dolor adipisci sint necessitatibus minima earum, laboriosam provident, in error ipsum aspernatur mollitia itaque natus possimus.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Contact =(props) =>{
+    // setTimeout(() => {
+    //     props.history.push('/about')
+    // }, 2000)
+    
+    return(
+        <div className="container">
+            <h4 className="center">Contact</h4>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Blanditiis voluptates placeat obcaecati sequi neque dolor adipisci sint necessitatibus minima earum, laboriosam provident, in error ipsum aspernatur mollitia itaque natus possimus.</p>
+        </div>
+    )
+}
+
+export default Contact
